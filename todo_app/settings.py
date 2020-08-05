9f60ba2350c38809717be81106e2eaefbdf6ad74
fyung36 <fyung36@gmail.com>
2020-08-05 16:31:24 +0100

    modified

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #installed modules
+    'rest_framework',
+    #Apps
+    'api',
 ]
 
 MIDDLEWARE = [
