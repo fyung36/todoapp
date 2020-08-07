d23843e3c53c04b20d49fc3bf01f01f6c14b93e2
fyung36 <fyung36@gmail.com>
2020-08-07 14:17:13 +0100

    added user to task

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     task_title = models.CharField(max_length=255)
-   # user = models.ForeignKey(User, on_delete=models.CASCADE())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     task_date = models.DateField(default=False,blank=True,null=True)
     task_time = models.DateTimeField()
     completed = models.BooleanField(default=False, blank=True,null=True)
